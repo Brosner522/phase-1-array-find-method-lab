adcda6c30f7a99825a3f3505b3cc416b723afbad
Brosner522 <rosnerbb@gmail.com>
2021-04-06 19:47:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+superbowlWin = (record) => {
+    //Finding what year the Chiefs won.
+    const result = record.find( record => record.result === "W" );
+    //Using ternary expression. 
+    return !!result ? result.year : undefined;
+  }
